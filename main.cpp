3454f6d7ee5f5f4bc82368c2d9e00b17b960db9b
DenysDanov <57838664+DenysDanov@users.noreply.github.com>
2020-02-20 18:19:22 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class Kadr{
+    public:
+    char* name;
+    int tseh,rozriad;
+    Kadr();
+    Kadr(char* n,int a,int b){
+        name = n;
+        tseh = a;
+        rozriad = b;
+    }
+    Kadr(const Kadr&);
+    ~Kadr(){}
+    void set(char* n,int a,int b){
+        name = n;
+        tseh = a;
+        rozriad = b;
+    }
+    void setName(char* n){
+        name = n;
+    }
+    void setTseh(int a){
+        tseh = a;
+    }
+    void setRozriad(int b){
+        rozriad = b;
+    }
+    char* getName(){
+        return name;
+    }
+    int getTseh(){
+        return tseh;
+    }
+    int getRozriad(){
+        return rozriad;
+    }
+};
+
+int main(){
+    Kadr k("Gus",0,6);
+    cout<<k.getName()<<endl
+        <<k.getTseh()<<endl
+        <<k.getRozriad()<<endl;
+    
+    
+}
